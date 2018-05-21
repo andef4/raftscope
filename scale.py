a7c54d293b171b340179394776cafacb1c9a58b2
fotcorn <fotcorn@gmail.com>
2018-05-21 16:39:18 +0200

    scale: fix text orientation and insert relative path to pdf files

--- scale.py
+++ scale.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import shutil
+import re
 
 def main():
     if len(sys.argv) != 2:
@@ -18,6 +19,25 @@ def main():
             os.path.join(input_dir, f),
             os.path.join(output_dir, f.replace('svg', 'pdf'))
         ))
+
+        output_file = os.path.join(output_dir, f.replace('svg', 'pdf_tex'))
+        with open(output_file) as f:
+            text = f.read()
+
+        for match in re.finditer(r'\\put\(\d\.\d{8},(\d\.\d{8})\)', text):
+            x = float(match.group(1))
+            x -= 0.02
+            if x < 0:
+                x = '{0:.7f}'.format(x)
+            else:
+                x = '{0:.8f}'.format(x)
+            text = text[:match.start(1)] + x + text[match.end(1):]
+
+        text = re.sub(r'(\d+.pdf)', input_dir + r'/\1', text)
+
+        with open(output_file, 'w') as f:
+            f.write(text)
+
 
 if __name__ == '__main__':
     main()
